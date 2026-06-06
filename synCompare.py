5de8237531834bf9183f66f40ca7106506942aed
Adam T'Koy <70171708+AdamTKoy@users.noreply.github.com>
2026-06-06 13:48:45 -0500

    Update synCompare.py

--- synCompare.py
+++ synCompare.py
@@ -1,5 +1,4 @@
 # Function to evaluate GROUPINGS of synonyms and feature codes
-
 import functions
 
 print("This function will compare two groupings of synonyms/features and return only those features that exist in both AND are compatible with the specified models in MMAC.")
@@ -10,4 +9,4 @@ M = input("Models: ")
 result, removed = functions.syn_compare(s1, s2, M)
 
 print("Features in both groupings and compatible with models: ", result)
-print("Removed features: ", removed)
+print("Removed (symmatric difference) features: ", removed)
